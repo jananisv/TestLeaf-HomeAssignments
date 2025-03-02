4f7127f0c0b678d316d3268c4907c5816f47d4b4
jananisv <67401111+jananisv@users.noreply.github.com>
2025-03-02 17:09:51 +0530

    Add files via upload
    Day2

---
+++ day2/Day2ClassRoomActivityOddnumbers.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+public class Day2ClassRoomActivityOddnumbers {
+
+	 public static void main(String[] args) {
+		
+		for (int i = 1; i <=10; i++) {
+			
+			if (i%2==1) {
+				System.out.println("Odd Numbers are:"+i);
+				
+			}
+			
+		}
+	}
+}
+
+
+
+	
