c0c3a448fb9538bd5f8a1cee910cfd5b194407ec
jananisv <67401111+jananisv@users.noreply.github.com>
2025-03-02 15:03:14 +0530

    Add files via upload
    Day1Janani

---
+++ day1/Day1HomeAssignment.java
@@ -0,0 +1,51 @@
+package week1.day1;
+
+public class Day1HomeAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 int regNO=777777;
+		 
+		 boolean isThisABike=true;
+		 
+		 long mobilenumber=807246543l;
+		 
+		 String modelname="HeroHonda";
+		 
+		 char logo='H';
+		 
+		 boolean doesTheDriverHasLisence=true;
+		 
+		//Print
+		 
+		 System.out.println(logo);
+		 
+		 System.out.println(doesTheDriverHasLisence);
+		 
+		 System.out.println(mobilenumber);
+		 
+		 System.out.println(isThisABike);
+		 
+		 System.out.print(modelname);
+		 
+		 System.out.print(regNO);
+		 
+		 System.out.println("Vehicle No:"+regNO+"\n"+"Name of the Vehicle :- "+modelname+"");
+		 		
+	
+		 
+	//	 System.out.println("Vechile Numberis"+regNo +" "+"Name of the vechile: "+modelname);
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+
+	}
+
+}
